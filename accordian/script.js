37e05778910f6c4ff1cdce1adcf4c00eeeac2fa7
Prasanth A R <73158535+ptech12@users.noreply.github.com>
2024-01-07 18:00:23 +0530

    JS logic broken needs fixes

--- accordian/script.js
+++ accordian/script.js
@@ -1,1 +1,16 @@
 // Constants and variables
+const accordian = document.getElementsByClassName('content-container');
+const title = document.getElementsByClassName('accordian-title');
+
+for ( let i = 0; i < accordian.length; i++){
+    // event listneres for each of these
+    console.log(accordian);
+    accordian[i].addEventListener('click', function () {
+
+        // by using THIS keyword reference the obj by calling the object
+        this.classList.toggle('active');
+
+
+         
+    })
+}
