e54892123e211e71fae76b1f6406f127c0630310
Prasanth A R <73158535+ptech12@users.noreply.github.com>
2024-02-06 17:38:37 +0530

    feat(app): implement and test bubbleSort algorithm

--- algorithms/bubleSort.js
+++ algorithms/bubleSort.js
@@ -1,1 +1,16 @@
-console.log('hello');
+function bubbleSort(arr){
+    /* for i..n => for j..n-1-i */
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - 1 - i; j++) {
+            /* swap */
+            if(arr[j] > arr[j + 1]){
+                const temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[i] = temp;
+            }
+            
+        }
+    }
+}
+
+export default bubbleSort;
