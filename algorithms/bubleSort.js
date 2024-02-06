be355b38e8da600116f3a39a2d3cf48dc1dbaee2
Prasanth A R <73158535+ptech12@users.noreply.github.com>
2024-02-06 17:52:47 +0530

    feat(app): bubbleSort algo is working

--- algorithms/bubleSort.js
+++ algorithms/bubleSort.js
@@ -1,16 +1,18 @@
 function bubbleSort(arr){
     /* for i..n => for j..n-1-i */
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < arr.length - 1 - i; j++) {
+    for (let i = 0; i < arr.length; ++i) {
+        for (let j = 0; j < arr.length - 1 - i; ++j) {
             /* swap */
             if(arr[j] > arr[j + 1]){
                 const temp = arr[j];
                 arr[j] = arr[j+1];
-                arr[i] = temp;
+                arr[j+1] = temp;
             }
             
         }
     }
+    console.log(arr);
+    return arr;
 }
 
-export default bubbleSort;
+module.exports = bubbleSort;
